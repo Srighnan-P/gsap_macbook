a8c48a89feafbfa6a60a6d52fbf05f8a3d42bb3a
Srighnan Pitchika <srighnanpitchika@gmail.com>
2025-11-30 16:36:07 +0530

    Implement hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import Image from 'next/image'
+import React, {useEffect, useRef} from 'react'
+
+
+const Hero = () => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.playbackRate = 2;
+        }
+    }, []);
+
+    return (
+        <section id='hero'>
+            <div>
+                <h1>MacBook Pro M4</h1>
+                <Image src="/title.png" alt="MacBook Title" width={700} height={400} />
+            </div>
+            <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline/>
+            <button>Buy</button>
+            <p>From $1599 or $133/mo for 12 months</p>
+        </section>
+    )
+}
+
+export default Hero
